7f7bec4c9fabff3d29a950782111803b8095bc1a
enrahsn <enrahsn@gmail.com>
2024-04-18 19:07:50 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'views/screens/on_boarding.dart';
+void main() {
+  runApp(const Market());
+}
+class Market extends StatelessWidget {
+  const Market({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Market',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Cairo',
+        useMaterial3: true,
+      ),
+      home: const OnBoarding(),
+
+    );
+  }
+}
+/*
+https://github.com/enrahsn/ecomerce_hmz.git
+*/
