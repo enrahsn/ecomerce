720b2c29d7649ca93379ebf7a14823fb7527347a
enrahsn <enrahsn@gmail.com>
2024-04-19 13:47:33 +0200

    onPageChanged

---
+++ lib/models/core/OnBoarding/index.dart
@@ -0,0 +1,5 @@
+export 'boarding_btn.dart';
+export 'dot_boarding.dart';
+export 'on_boarding_model.dart';
+export 'slider.dart';
+
--- lib/models/core/OnBoarding/slider.dart
+++ lib/models/core/OnBoarding/slider.dart
@@ -1,52 +1,56 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../../controller/boarding_ctrl.dart';
 import '../../../models/constant/colors.dart';
 import '../../../models/data/data_source/static/static.dart';
 
-class MySlider extends StatelessWidget {
+class MySlider extends GetView<BoardingCtrlImp> {
   const MySlider({super.key});
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
-                itemCount: onBoardingList.length,
-                itemBuilder: (context, i) => Column(children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    onBoardingList[i].title!,
-                    // style: Theme.of(context).textTheme.displayMedium,
-                    style:
-                        const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Image.asset(
-                    onBoardingList[i].image!,
-                    // width: 200,
-                    height: 230,
-                    fit: BoxFit.fill,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      child: Text(
-                        onBoardingList[i].description!,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            height: 2,
-                            color: AppColor.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17),
-                      )),
-                      // const SizedBox(height: 10),
-                ]),
-              );
+      onPageChanged: (val) {
+        // controller.changeIndex(val);
+        print(val);
+      },
+      itemCount: onBoardingList.length,
+      itemBuilder: (context, i) => Column(children: [
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          onBoardingList[i].title!,
+          // style: Theme.of(context).textTheme.displayMedium,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Image.asset(
+          onBoardingList[i].image!,
+          // width: 200,
+          height: 230,
+          fit: BoxFit.fill,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Text(
+              onBoardingList[i].description!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  height: 2,
+                  color: AppColor.grey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17),
+            )),
+        // const SizedBox(height: 10),
+      ]),
+    );
   }
 }
