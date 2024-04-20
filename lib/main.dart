15611b092872d7aca1a75f9ba32814f0a4baf273
enrahsn <enrahsn@gmail.com>
2024-04-20 11:35:57 +0200

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'models/constant/colors.dart';
 import 'models/core/route.dart';
 import 'views/screens/on_boarding.dart';
 void main() {
   runApp(const Market());
 }
 class Market extends StatelessWidget {
-  const Market({super.key});
+      const Market({super.key, 
+
+  });
+
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Market',
       theme: ThemeData(
-        headline1:TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: AppColor.black,),
+        headline1:const TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: AppColor.black,),
+        headText1:const TextStyle(fontWeight: FontWeight.bold,fontSize: 17,color: AppColor.grey,height: 2),
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
         fontFamily: 'Cairo',
--- lib/models/constant/colors.dart
+++ lib/models/constant/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 class AppColor{
   static const grey = Color(0xff8e8e8e);
+  static const black = Color(0xff000000);
 }
 const kPrimaryColor = Color(0xff5d81df);
 const kPrimaryColor0 = Color(0xff62FCD7);
